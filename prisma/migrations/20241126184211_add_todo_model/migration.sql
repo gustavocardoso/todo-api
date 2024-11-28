de42d072f8787559bdd4114ee2e0e971820ac4f8
Gustavo Cardoso <gustavocardoso@gmail.com>
2024-11-28 01:13:03 -0800

    Remove unused files

--- prisma/migrations/20241126184211_add_todo_model/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Todo" (
-    "id" TEXT NOT NULL,
-    "title" VARCHAR(255) NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
-
-    CONSTRAINT "Todo_pkey" PRIMARY KEY ("id")
-);
